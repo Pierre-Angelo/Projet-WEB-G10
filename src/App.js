d8ead49bd4c51ca38406ff2cfc4ff63d9873fd25
Sol-Bb <solennebaby36@gmail.com>
2024-03-25 16:52:38 +0100

    Frontend
    Avancement du frontend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import "./styles.css"; // Assurez-vous d'avoir le fichier CSS approprié
+import "./App.css"; // Assurez-vous d'avoir le fichier CSS approprié
+
 
 const Flashcard = ({ question, answer }) => {
   return (
@@ -22,30 +23,236 @@ const Theme = ({name,handleClick}) => {
 const App = () => {
   const db = {
     DCO : [
-      { id: 1, question: "DCO 1", answer: "Réponse 1" },
+      { id: 1, question: "DCO 1", answer: "Réponse  DCo1" },
       { id: 2, question: "DCO 2", answer: "Réponse 2" },
       { id: 3, question: "DCO 3", answer: "Réponse 3" },
     ],
     MAS : [
-      { id: 1, question: "MAS 1", answer: "Réponse 1" },
+      { id: 1, question: "MAS 1", answer: "Réponse MaS1" },
       { id: 2, question: "MAS 2", answer: "Réponse 2" },
       { id: 3, question: "MAS 3", answer: "Réponse 3" },
     ],
     NAS : [
-      { id: 1, question: "NAS 1", answer: "Réponse 1" },
+      { id: 1, question: "NAS 1", answer: "Réponse NAs1" },
       { id: 2, question: "NAS 2", answer: "Réponse 2" },
       { id: 3, question: "NAS 3", answer: "Réponse 3" },
     ],
     
   };
 
+  const Block = () => {
+    return (
+      <div>
+      <div className="app">
+      <div className="flashcard-library">
+        <h2>Flashcards</h2>
+          <div>
+            
+          </div>
+
+      </div>
+      </div>
+      </div>
+    )
+  }
+
+  const HandleNewThema = () => {
+    const [inputThema, setInputThema] = useState('');
+    const [addToData, setAddToData] = useState('');
+
+    const handleChange = (event) => {
+      setInputThema(event.target.value);
+    };
+
+    const handleThema = () => {
+      setAddToData(inputThema);
+      setInputThema('');
+    };
+
+    return (
+      <div>
+        <input type="text"
+        value={inputThema}
+        onChange={handleChange}
+        placeholder="Type your new Thema..."
+        />
+        <button onClick={handleThema}>Ajouter</button>
+        <p>New Thema : {addToData}</p>
+      </div>
+    );
+  }
+
+  const Thema = () => { return(
+    <li className='bullets'>
+      <label htmlFor="theme">Theme : </label>
+      <input type="text" id="theme" name="theme"/>
+    </li>
+    )}
+
+  const Question = () => { return(
+    <li className='bullets'>
+      <label htmlFor="question">Question : </label>
+      <input type="text" id="question" name="question"/>
+    </li>
+    )}
+    
+    const Answer = () => {return(
+    <li className='bullets'>
+      <label htmlFor="answer">Answer : </label>
+      <input type="text" id="answer" name="answer"/>
+    </li>
+    )}
+
   const  [activeTheme, setActiveTheme] = useState([])
 
-  const handleDeleteFlashcard = (id) => {};
+  //const handleDeleteFlashcard = (id) => {};
 
-  const handleAddFlashcard = () => {};
+  const HandleAnswer = () => {
+    const [visible, setVisible] = useState(false);
 
-  const handleReviewFlashcards = () => {};
+    const handleHide = () => {
+      setVisible(false);
+    };
+
+    const handleShow = () => {
+      setVisible(true);
+    };
+
+    return (
+      <div>
+      {visible && <div>
+        {activeTheme.map((flashcard) => (
+                <div><button onClick={handleHide}><Flashcard
+                key={flashcard.id}
+                question={flashcard.answer}         
+                /></button>
+               </div>))}
+      </div>}
+
+      {!visible && <div>
+                {activeTheme.map((flashcard) => (
+                <div><button onClick={handleShow}><Flashcard
+                key={flashcard.id}
+                question={flashcard.question}
+                /></button><button onClick={handleHide}>
+                Supprimer une flashcard
+              </button>
+               </div>))}</div>}
+      </div>
+    );
+  };
+
+  const NewFlashcard = () => {
+    const [isVisibl, setIsVisibl] = useState(false);
+    const [study, setStudy] = useState(false);
+    
+    const handleStudy = () => {
+      setStudy(true)
+    };
+
+    const handleNotStudy = () => {
+      setStudy(false)
+    };
+
+    const handleHide = () => {
+      setIsVisibl(false)
+    }
+  
+    const handleShow = () => {
+      setIsVisibl(true)
+    }
+  
+    return(
+      <div>
+  
+        {!isVisibl && !study &&
+        <div><hr className="separator" />
+        
+        <div className="app">
+        <Block/>
+        <HandleAnswer/>
+        <button onClick={handleShow}>
+           Ajouter une flashcard
+        </button>
+        </div>
+        <div>
+          <button id = 'revise' className="revise" onClick={handleStudy}>
+            Réviser
+          </button>
+        </div>
+        </div>}
+
+        {!isVisibl && study &&
+
+        <div><hr className="separator" /><p>interface pour étudier</p>
+        <button onClick={handleNotStudy}>
+          Terminer
+        </button></div>}
+        
+        {isVisibl &&
+           <div><hr className="separator" />
+           <Thema/>
+           <Question/>
+           <Answer/>
+           <button onClick={handleHide}>
+           Cancel
+           </button>
+     </div>}
+      </div>
+    )
+  };
+
+  const CreateNewThema = () => {
+    const [isVisib, setIsVisib] = useState(false);
+    
+    const handleHide = () => {
+      setIsVisib(false)
+    }
+  
+    const handleShow = () => {
+      setIsVisib(true)
+    }
+  
+    return(
+      <div>
+        {!isVisib && 
+        <button className="addFC" onClick={handleShow}>
+            Créer un nouveau thème
+        </button>}
+  
+        {isVisib &&
+        <div>
+        <HandleNewThema/>
+        <button onClick={handleHide}>
+        Cancel
+        </button>
+      </div>
+            }
+      </div>
+    )
+  }
+
+  const Toggle = () => {
+    const [isVisible, setIsVisible] = useState(true);
+    
+    const handleHide = () => {
+      setIsVisible(false)
+    }
+  
+    return(
+        <div><CreateNewThema/>
+        <button onClick={handleHide}>
+            Hide
+        </button>
+        
+  
+        {isVisible &&
+            <div> 
+                <NewFlashcard/> 
+            </div>}
+      </div>
+    )
+  }
 
   return (
     <div className="app">
@@ -57,29 +264,11 @@ const App = () => {
             handleClick={() => setActiveTheme(db[theme])}
             />
           ))}
-        <button>
-          Créer un nouveau thème
-        </button>
       </div>
-      <hr className="separator" />
-      <div className="flashcard-library">
-        <h2>Bibliothèque de flashcards</h2>
-        {activeTheme.map((flashcard) => (
-          <Flashcard
-            key={flashcard.id}
-            question={flashcard.question}
-          />
-        ))}
-        <div className="controls">
-          <button className="addFC" onClick={handleAddFlashcard}>ajouter une flashcard</button>
-          <button>
-            Supprimer une flashcard
-          </button>
-        </div>
-        <div>
-          <button onClick={handleReviewFlashcards}>Réviser</button>
-          </div>
-      </div>
+      <Toggle/>
+      {/*<NewFlashcard/><hr className="separator" />
+      => problème d'affichage à la réinitialisation
+      => CSS*/}
     </div>
   );
 };
