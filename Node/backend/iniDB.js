677e5036cf170b63a0d3093f79dbb690b77678a6
Rolgarel <raphael.dembik@gmail.com>
2024-03-25 17:26:11 +0100

    test real question

--- Node/backend/iniDB.js
+++ Node/backend/iniDB.js
@@ -7,10 +7,16 @@ var db = mongoose.connection;
 async function iniBase () {
     const base = await new Base({
         themeArray: [{
-            name: 'test',
+            name: 'PPC',
             cardArray: [
-                {question: 'q1', response: 'r1'},
-                {question: 'q2', response: 'r2'}
+                {
+                    question: 'Which part of the operating system decides which process will run next?',
+                    response: 'The scheduler'
+                },
+                {
+                    question: 'On a dual-core system, how many processes can run on a single processing core at the same time?',
+                    response: '1'
+                }
             ]
         },
         {
